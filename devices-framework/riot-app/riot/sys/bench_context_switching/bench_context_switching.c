38ade08e097ca215f7465047dfa70503af11d612
Gomez Julien <derwaan@gmail.com>
2019-06-04 16:23:01 +0200

    Change port

--- devices-framework/riot-app/riot/sys/bench_context_switching/bench_context_switching.c
+++ devices-framework/riot-app/riot/sys/bench_context_switching/bench_context_switching.c
@@ -3,18 +3,18 @@
 void bench_init(void)
 {
     // Init the GPIO
-    gpio_init(GPIO_PIN(PORT_C, 1), GPIO_OUT);
+    gpio_init(GPIO_PIN(PORT_C, 2), GPIO_OUT);
 }
 
 void bench_on(void)
 {
     // Set the GPIO up
-    gpio_set(GPIO_PIN(PORT_C, 1));
+    gpio_set(GPIO_PIN(PORT_C, 2));
 }
 
 void bench_off(void)
 {
     // Clear the GPIO
-    gpio_clear(GPIO_PIN(PORT_C, 1));
+    gpio_clear(GPIO_PIN(PORT_C, 2));
 }
 
